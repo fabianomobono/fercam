692195146031357a399c04b33fb4717a946645fb
fabianomobono <fabian.omobono@gmail.com>
2021-03-16 11:38:41 -0400

    edited installed apps

--- fercam/settings.py
+++ fercam/settings.py
@@ -49,8 +49,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
+    'storages',
+    'rest_framework',
+    'corsheaders',
 ]
+
 
 
 
