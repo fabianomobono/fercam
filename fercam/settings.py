026291eb83d0c98a802acd9cd822d40825fba75a
fabianomobono <fabian.omobono@gmail.com>
2021-03-16 00:03:29 -0400

    added allowed hosts2

--- fercam/settings.py
+++ fercam/settings.py
@@ -28,7 +28,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 API_KEY = os.getenv("GOOGLE_MAPS_API_KEY")
 GAS_API = os.getenv("GAS_API")
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the secret key used in production secret!!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
